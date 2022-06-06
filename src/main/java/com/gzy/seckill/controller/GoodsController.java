53bd6d70470f1187a229f7ab492d2016d87f54c0
高宗阳 <gaozongyang@dianzezixun.com>
2022-06-06 14:31:18 +0800

    秒杀基础实现

--- src/main/java/com/gzy/seckill/controller/GoodsController.java
+++ src/main/java/com/gzy/seckill/controller/GoodsController.java
@@ -2,18 +2,15 @@ package com.gzy.seckill.controller;
 
 import com.gzy.seckill.pojo.User;
 import com.gzy.seckill.service.IGoodsService;
-import com.gzy.seckill.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
-
-    @Autowired
-    private IUserService userService;
 
     @Autowired
     private IGoodsService goodsService;
@@ -23,5 +20,12 @@ public class GoodsController {
         model.addAttribute("user", user);
         model.addAttribute("goodsList", goodsService.findGoodsVo());
         return "goodsList";
+    }
+
+    @RequestMapping("/toDetail/{goodsId}")
+    public String toDetail(Model model, User user, @PathVariable Long goodsId) {
+        model.addAttribute("user", user);
+        model.addAttribute("goods", goodsService.findGoodsVoByGoodsId(goodsId));
+        return "goodsDetail";
     }
 }
--- src/main/java/com/gzy/seckill/service/IOrderService.java
+++ src/main/java/com/gzy/seckill/service/IOrderService.java
@@ -2,6 +2,8 @@ package com.gzy.seckill.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.gzy.seckill.pojo.Order;
+import com.gzy.seckill.pojo.User;
+import com.gzy.seckill.vo.GoodsVo;
 
 /**
  * <p>
@@ -13,4 +15,5 @@ import com.gzy.seckill.pojo.Order;
  */
 public interface IOrderService extends IService<Order> {
 
+    Order seckill(User user, GoodsVo goods);
 }
