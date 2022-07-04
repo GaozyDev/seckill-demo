eadbd45b630f283f2f00cbb6c14ba41b00b362b4
GaozyDev <670453367@qq.com>
2022-07-04 16:47:45 +0800

    验证码

--- src/main/java/com/gzy/seckill/controller/SecKillController.java
+++ src/main/java/com/gzy/seckill/controller/SecKillController.java
@@ -1,6 +1,7 @@
 package com.gzy.seckill.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.gzy.seckill.exception.GlobalException;
 import com.gzy.seckill.pojo.Order;
 import com.gzy.seckill.pojo.SeckillOrder;
 import com.gzy.seckill.pojo.User;
@@ -9,11 +10,20 @@ import com.gzy.seckill.service.IOrderService;
 import com.gzy.seckill.service.ISeckillOrderService;
 import com.gzy.seckill.vo.GoodsVo;
 import com.gzy.seckill.vo.RespBeanEnum;
+import com.wf.captcha.ArithmeticCaptcha;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @Controller
 @RequestMapping("/seckill")
 public class SecKillController {
@@ -26,6 +36,28 @@ public class SecKillController {
 
     @Autowired
     private IOrderService orderService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping(value = "/captcha")
+    public void verifyCode(User user, Long goodsId, HttpServletResponse response) {
+        if (user == null || goodsId < 0) {
+            throw new GlobalException(RespBeanEnum.REQUEST_ILLEGAL);
+        }
+        response.setContentType("image/jpg");
+        response.setHeader("Pargam", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        ArithmeticCaptcha captcha = new ArithmeticCaptcha(130, 32, 3);
+        redisTemplate.opsForValue().set("captcha:" + user.getId() + ":" + goodsId, captcha.text(), 300, TimeUnit.SECONDS);
+        try {
+            captcha.out(response.getOutputStream());
+        } catch (IOException e) {
+            log.error("验证码生成失败", e.getMessage());
+        }
+    }
 
     @RequestMapping("doSeckill")
     public String doSeckill(Model model, User user, Long goodsId) {
--- src/main/java/com/gzy/seckill/vo/RespBeanEnum.java
+++ src/main/java/com/gzy/seckill/vo/RespBeanEnum.java
@@ -15,7 +15,8 @@ public enum RespBeanEnum {
     MOBILE_ERROR(500211, "手机号码格式不正确"),
     BIND_ERROR(500212, "参数校验异常"),
     EMPTY_STOCK(500500, "库存不足"),
-    REPEAT_ERROR(500500, "该商品每人限购一件");
+    REPEAT_ERROR(500500, "该商品每人限购一件"),
+    REQUEST_ILLEGAL(500502, "请求非法，请重新尝试");
 
     private final Integer code;
     private final String message;
